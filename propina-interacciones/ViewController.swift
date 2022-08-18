1115e93e5f5714424e1188ea1d3293524dc8480f
Alumno <ulsa@D105.local>
2022-08-18 09:48:50 -0700

    propina

--- propina-interacciones/ViewController.swift
+++ propina-interacciones/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var porcentaje : Float = 20.0
+    
 
+    @IBOutlet weak var lblPorcentaje: UILabel!
+    @IBOutlet weak var lblPropinaTotal: UILabel!
+    @IBOutlet weak var lblPrecioTotal: UILabel!
+    @IBOutlet weak var sldPropinaOutlet: UISlider!
+    @IBOutlet weak var txtPrecioOutlet: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +30,21 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func doChangePorpina(_ sender: Any) {
+        porcentaje = (sldPropinaOutlet.value)
+        lblPorcentaje.text = "\(porcentaje)%"
+        
+        
+        let txtPrecio = txtPrecioOutlet.text!
+        let precio = Float(txtPrecio) ?? 0.0
+        let propina = precio * (porcentaje / 100.0)
+        lblPropinaTotal.text = "\(propina)"
+        
+        let total = precio + propina
+        lblPrecioTotal.text =  "\(total)"
+        
+    }
+
+    
 }
 
